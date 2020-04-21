78d3373a21cd1f4546455ec300ef619725780bea
Bavrovskyy <jetbsbs@gmail.com>
2020-04-21 09:05:15 +0300

    Add secton ADVANTAGES, NEWS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__slider-inner').slick({
         prevArrow: '<button type="button" class="slick__btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick__btn slick-next"></button>',
         infinite: false
